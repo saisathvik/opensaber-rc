8c2ce1e2b021a78a81863b160bce54317f5e87b0
Sai Sathvik <saisathviky@sahaj.ai>
2021-07-13 18:44:22 +0530

    role management

--- java/registry/src/main/java/io/opensaber/registry/util/KeycloakAdminUtil.java
+++ java/registry/src/main/java/io/opensaber/registry/util/KeycloakAdminUtil.java
@@ -1,25 +1,35 @@
 package io.opensaber.registry.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.opensaber.registry.exception.DuplicateRecordException;
 import io.opensaber.registry.exception.EntityCreationException;
-import org.apache.catalina.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RolesResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.GroupRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
-import javax.swing.text.html.Option;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.Response;
 
 
@@ -33,18 +43,86 @@ public class KeycloakAdminUtil {
     private String adminClientId;
     private String authURL;
     private final Keycloak keycloak;
+    private DefinitionsManager definitionsManager;
+    private String roleBaseFilePath;
+    private String groupDefinitionDir;
+
+
+    @NoArgsConstructor
+    @Getter
+    static
+    class RoleDefinition {
+        private String name;
+        private String description;
+    }
+
+    @NoArgsConstructor
+    @Getter
+    static
+    class GroupDefinition {
+        private String name;
+        private List<String> realmRoles;
+    }
 
     @Autowired
     public KeycloakAdminUtil(
             @Value("${keycloak.realm}") String realm,
             @Value("${keycloak-admin.client-secret}") String adminClientSecret,
             @Value("${keycloak-admin.client-id}") String adminClientId,
-            @Value("${keycloak.auth-server-url}") String authURL) {
+            @Value("${keycloak.auth-server-url}") String authURL,
+            @Value("${groups.roles.base-file}") String roleBaseFilePath,
+            @Value("${groups.definition-dir}") String groupDefinitionDir,
+            @Autowired DefinitionsManager definitionsManager) {
         this.realm = realm;
         this.adminClientSecret = adminClientSecret;
         this.adminClientId = adminClientId;
         this.authURL = authURL;
         this.keycloak = buildKeycloak();
+        this.roleBaseFilePath = roleBaseFilePath;
+        this.groupDefinitionDir = groupDefinitionDir;
+        this.definitionsManager = definitionsManager;
+    }
+
+    @PostConstruct
+    void loadEntityRolesAndGroups() throws Exception {
+        ObjectMapper m = new ObjectMapper();
+        List<RoleDefinition> roleDefinitions = m.readValue(
+                ResourceUtils.getFile(roleBaseFilePath),
+                new TypeReference<List<RoleDefinition>>() {}
+        );
+
+        File entityGroupDir = ResourceUtils.getFile(groupDefinitionDir);
+        if (!entityGroupDir.isDirectory()) {
+            throw new Exception("Expected directory");
+        }
+
+
+        for (String entityType: definitionsManager.getAllKnownDefinitions()) {
+            for (RoleDefinition roleDefinition: roleDefinitions) {
+                String roleName = getRealmRoleWithPrefix(entityType, roleDefinition.getName());
+                createRealmRole(roleName, roleDefinition.getDescription());
+            }
+            Optional<File> entityGroupDefinitionsFile = Arrays.stream(Objects.requireNonNull(entityGroupDir.listFiles()))
+                    .filter(f -> f.getName().startsWith(entityType))
+                    .findFirst();
+
+            if (entityGroupDefinitionsFile.isPresent()) {
+                List<GroupDefinition> groupDefinitions = m.readValue(
+                        entityGroupDefinitionsFile.get(),
+                        new TypeReference<List<GroupDefinition>>() {}
+                );
+
+                for (GroupDefinition groupDefinition: groupDefinitions) {
+                    createGroup(
+                            groupDefinition.getName(),
+                            groupDefinition.getRealmRoles()
+                                    .stream()
+                                    .map(r -> getRealmRoleWithPrefix(entityType, r))
+                                    .collect(Collectors.toList())
+                    );
+                }
+            }
+        }
     }
 
     private Keycloak buildKeycloak() {
@@ -73,7 +151,7 @@ public class KeycloakAdminUtil {
         if (response.getStatus() == 201) {
             logger.info("Response |  Status: {} | Status Info: {}", response.getStatus(), response.getStatusInfo());
             logger.info("User ID path" + response.getLocation().getPath());
-            String userID = response.getLocation().getPath().replaceAll(".*/([^/]+)$", "$1");
+            String userID = getCreatedId(response);
             logger.info("User ID : " + userID);
             return userID;
         } else if (response.getStatus() == 409) {
@@ -111,5 +189,46 @@ public class KeycloakAdminUtil {
         keycloak.realm(realm).groups().groups().stream()
                 .filter(g -> g.getName().equals(groupName)).findFirst()
                 .ifPresent(g -> keycloak.realm(realm).users().get(user.getId()).joinGroup(g.getId()));
+    }
+
+    private void createGroup(String groupName, List<String> realmRoles) throws EntityCreationException {
+        GroupRepresentation groupRepresentation = new GroupRepresentation();
+        groupRepresentation.setName(groupName);
+        Response response = keycloak.realm(realm).groups().add(groupRepresentation);
+        if (!Arrays.asList(201, 409).contains(response.getStatus())) {
+            throw new EntityCreationException("Error creating group");
+        }
+
+        List<RoleRepresentation> roles = keycloak.realm(realm).roles().list().stream()
+                .filter(rp -> realmRoles.contains(rp.getName()))
+                .collect(Collectors.toList());
+
+        if (response.getStatus() == 201) {
+            String groupId = getCreatedId(response);
+            keycloak.realm(realm).groups().group(groupId)
+                    .roles().realmLevel().add(roles);
+        }
+    }
+
+    private void createRealmRole(String name, String description) {
+        RoleRepresentation roleRepresentation = new RoleRepresentation();
+        roleRepresentation.setName(name);
+        roleRepresentation.setDescription(description);
+        RolesResource rolesResource = keycloak.realm(realm).roles();
+        try {
+            rolesResource.create(roleRepresentation);
+        } catch (ClientErrorException e) {
+            if (e.getResponse().getStatus() != 409) {
+                throw e;
+            }
+        }
+    }
+
+    private String getCreatedId(Response response) {
+        return response.getLocation().getPath().replaceAll(".*/([^/]+)$", "$1");
+    }
+
+    private String getRealmRoleWithPrefix(String entityType, String baseRole) {
+        return entityType.toLowerCase() + "-" + baseRole;
     }
 }
